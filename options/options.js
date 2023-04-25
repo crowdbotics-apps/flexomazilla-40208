4d98a14d19c2cd94c7be07e083cb5aa4c5a81ac0
Crowdbotics <team@crowdbotics.com>
2023-04-25 17:59:51 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "restless-snow-40208",
+  url: "https://restless-snow-40208.botics.co",
+  api: "https://restless-snow-40208.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
